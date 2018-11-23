e6f437c5dfd65bdb37840d6280e4158bd3c27592
Mohamed Elmi <maxto024@gmail.com>
2018-11-23 05:54:08 +0400

    third Commit

--- App/Containers/LaunchScreen.js
+++ App/Containers/LaunchScreen.js
@@ -1,29 +1,51 @@
-import React, { Component } from 'react'
-import { ScrollView, Text, Image, View } from 'react-native'
-import { Images } from '../Themes'
 
-// Styles
-import styles from './Styles/LaunchScreenStyles'
-
+import React,{Component} from 'react'
+import {Text,View,} from 'react-native'
+import Carousel, { Pagination }  from 'react-native-snap-carousel';
+import styles,{itemWidth,sliderWidth} from './Styles/LaunchScreenStyles';
+import {ENTRIES1} from '../Static/Enteries'
+import { Colors } from '../Themes';
+import SliderEntry from '../Components/SliderEntry'
 export default class LaunchScreen extends Component {
-  render () {
-    return (
-      <View style={styles.mainContainer}>
-        <Image source={Images.background} style={styles.backgroundImage} resizeMode='stretch' />
-        <ScrollView style={styles.container}>
-          <View style={styles.centered}>
-            <Image source={Images.launch} style={styles.logo} />
-          </View>
-
-          <View style={styles.section} >
-            <Image source={Images.ready} />
-            <Text style={styles.sectionText}>
-              This probably isn't what your app is going to look like. Unless your designer handed you this screen and, in that case, congrats! You're ready to ship. For everyone else, this is where you'll see a live preview of your fully functioning app using Ignite.
-            </Text>
-          </View>
-
-        </ScrollView>
-      </View>
-    )
+  state={
+    entries:ENTRIES1,
+    slider1ActiveSlide: 1
   }
+ 
+    _renderItemWithParallax ({item, index}, parallaxProps) {
+      return (
+          <SliderEntry
+            data={item}
+            even={(index + 1) % 2 === 0}
+            parallax={true}
+            parallaxProps={parallaxProps}
+          />
+      );
+  }
+    render () {
+      const { slider1ActiveSlide } = this.state;
+
+      return (
+          <View style={styles.exampleContainer}>
+              <Text style={styles.title}>{`Example 1`}</Text>
+              <Text style={styles.subtitle}>example</Text>
+              <Carousel
+                ref={c => this._slider1Ref = c}
+                data={ENTRIES1}
+                renderItem={this._renderItemWithParallax}
+                sliderWidth={sliderWidth}
+                itemWidth={itemWidth}
+                hasParallaxImages={true}
+                firstItem={1}
+
+                // inactiveSlideShift={20}
+                containerCustomStyle={styles.slider}
+                contentContainerCustomStyle={styles.sliderContentContainer}
+                onSnapToItem={(index) => this.setState({ slider1ActiveSlide: index }) }
+              />
+        
+          </View>
+      );
+  }
+
 }
